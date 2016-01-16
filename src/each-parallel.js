21e542c9da3471bb88223c829b069196c37f3185
Jarid Margolin <jaridmargolin@gmail.com>
2016-01-16 14:25:32 -0800

    Add eachParallel method.

---
+++ src/each-parallel.js
@@ -0,0 +1,62 @@
+/*!
+ * each-parallel.js
+ *
+ * Copyright (c) 2014
+ */
+
+
+define(function () {
+
+
+/* -----------------------------------------------------------------------------
+ * eachParallel
+ * ---------------------------------------------------------------------------*/
+
+/**
+ * Applies the function iterator to each item in arr, in parallel. The iterator
+ * is called with an item from the list, and a callback for when it has finished.
+ * If the iterator passes an error to its callback, the main callback (for the
+ * each function) is immediately called with the error.
+ *
+ * Note, that since this function applies iterator to each item in parallel,
+ * there is no guarantee that the iterator functions will complete in order.
+ *
+ * @example
+ * aflow.eachParallel([1, 2], function (val, next) {
+ *   next(val);
+ * }, function (err, results) {
+ *   if (err) {
+ *     console.log('Bummer dude');
+ *   }
+ *
+ *   doSomething(results[0], results[1]);
+ * });
+ *
+ * @public
+ * @params {array} array - The array to loop through.
+ * @params {function} iterator - The iterator called for each item in
+ *   the array.
+ * @params {function} callback - An optional callback to call once all
+ *   functions have completed running, or an error has been thrown.
+ */
+return function (obj, iterator, done) {
+  var length = obj.length;
+  var results = [];
+  var completed = 0;
+
+  var callback = function (err) {
+    results.push(Array.prototype.slice.call(arguments, 1));
+    completed ++;
+
+    if (err || completed === length) {
+      done(err, results);
+    }
+  };
+
+  for (var i = 0; i < length; i++) {
+    iterator(obj[i], callback);
+  }
+};
+
+
+});
---
+++ test/each-parallel.js
@@ -0,0 +1,39 @@
+/*!
+ * test/each-parallel.js
+ * 
+ * Copyright (c) 2014
+ */
+
+define([
+  'proclaim',
+  'sinon',
+  'each-parallel'
+], function (assert, sinon, eachParallel) {
+
+
+/* -----------------------------------------------------------------------------
+ * test
+ * ---------------------------------------------------------------------------*/
+
+describe('each-parallel.js', function () {
+
+  it('Should call onComplete with results.', function () {
+    eachParallel(['1', '2'], function (val, callback) {
+      callback(null, val);
+    }, function (err, results) {
+      assert.deepEqual(results, ['1', '2']);
+    });
+  });
+
+  it('Should call onComplete with error.', function () {
+    eachParallel(['1', '2'], function (val, callback) {
+      return callback(val === '2' ? 'fail' : null, val);
+    }, function (err, results) {
+      assert.equal(err, 'fail');
+    });
+  });
+
+});
+
+
+});
